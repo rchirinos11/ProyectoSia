ef6ebbb0926ba695e2cf89d2f397ab79df429b2c
RJ-code2020 <rjoropeza@pucp.edu.pe>
2021-05-04 21:26:52 -0500

    Repository 4 agregados
    De agregó los Repository de EducationalObjective, Faculty, Speciality y StudentResult.

---
+++ src/main/java/pe/edu/pucp/sia/repository/FacultyRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.sia.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pe.edu.pucp.sia.model.Faculty;
+
+public interface FacultyRepository extends CrudRepository<Faculty,Integer>{
+
+}
---
+++ src/main/java/pe/edu/pucp/sia/repository/SpecialityRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.sia.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pe.edu.pucp.sia.model.Speciality;
+
+public interface SpecialityRepository extends CrudRepository<Speciality,Integer>{
+
+}
