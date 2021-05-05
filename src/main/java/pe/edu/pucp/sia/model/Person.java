db7b9db959de0d0fdfb7e5a459711a6ea41b1f9b
RJ-code2020 <rjoropeza@pucp.edu.pe>
2021-05-04 21:25:18 -0500

    Persona Id
    Se agregó el Parametro para que el id en la base de datos siga el formato id_nombre_clase

--- src/main/java/pe/edu/pucp/sia/model/Person.java
+++ src/main/java/pe/edu/pucp/sia/model/Person.java
@@ -1,9 +1,12 @@
 package pe.edu.pucp.sia.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.Getter;
@@ -12,11 +15,14 @@ import lombok.Setter;
 
 @Entity
 public class Person {
-	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	@Id @Column(name="id_person") @GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	private String code;
 	private String name;
 	private String pLastName;
 	private String mLastName;
 	private String email;
+	//@ManyToOne						//Esto lo agregan para sus FK
+	//@JoinColumn(name="id_person")		//Esto lo agregan para sus FK
+	//private ClasePadre clasePadre;	//Esto lo agregan para sus FK
 }
