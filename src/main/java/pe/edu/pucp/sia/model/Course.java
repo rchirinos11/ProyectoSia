64401f92529813e90acea51ffbd6bbb24ee26cf1
Robert <rchirinos@inntechcompany.com>
2021-05-04 19:19:20 -0500

    Agregar @Column a Modelos
    Para indicar a que columna de base de datos apunta

--- src/main/java/pe/edu/pucp/sia/model/Course.java
+++ src/main/java/pe/edu/pucp/sia/model/Course.java
@@ -1,5 +1,6 @@
 package pe.edu.pucp.sia.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Entity @Getter @Setter
 public class Course {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_course")
 	private Integer id;
 	private String code;
 	private String name;
