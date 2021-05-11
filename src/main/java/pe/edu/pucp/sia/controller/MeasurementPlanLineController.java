f3230931ec1a265a014390b798e02faf7c7fdcaa
Robert <rchirinos@inntechcompany.com>
2021-05-10 22:38:16 -0500

    RequestBody en MPLController

--- src/main/java/pe/edu/pucp/sia/controller/MeasurementPlanLineController.java
+++ src/main/java/pe/edu/pucp/sia/controller/MeasurementPlanLineController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,12 +27,13 @@ public class MeasurementPlanLineController {
 	}
 	
 	@PostMapping("/create")
-	public ResponseEntity<Object> createMeasurementPlanLine(MeasurementPlanLine m){
+	public ResponseEntity<Object> createMeasurementPlanLine(@RequestBody MeasurementPlanLine m){
 		return ResponseEntity.status(HttpStatus.CREATED).body(mPlanLineService.createMeasurementPlanLine(m));
 	}
 	
 	@PostMapping("/update")
-	public ResponseEntity<Object> updateMeasurementPlanLine(MeasurementPlanLine m){
+	public ResponseEntity<Object> updateMeasurementPlanLine(@RequestBody MeasurementPlanLine m){
+		System.out.println(m);
 		return ResponseEntity.status(HttpStatus.OK).body(mPlanLineService.updateMeasurementPlanLine(m));
 	}
 	
