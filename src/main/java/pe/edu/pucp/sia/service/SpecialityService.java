b27738014da7513e87b01d71612acb98a3c5cac4
RJ-code2020 <rjoropeza@pucp.edu.pe>
2021-05-11 23:25:27 -0500

    Add listbyFaculty and listbySpeciality
    Se agregó para las especialidades el listar por facultad.
    Se agregó para los resultados de estudiante el listar por especialidad.

--- src/main/java/pe/edu/pucp/sia/service/SpecialityService.java
+++ src/main/java/pe/edu/pucp/sia/service/SpecialityService.java
@@ -4,6 +4,7 @@ import pe.edu.pucp.sia.model.Speciality;
 
 public interface SpecialityService {
 	public Iterable<Speciality> listAll();
+	public Iterable<Speciality> listByFaculty(Integer id);
 	public String createSpeciality(Speciality s);
 	public String updateSpeciality(Speciality s);
 	public String deleteSpeciality(Integer id);
--- src/main/java/pe/edu/pucp/sia/service/StudentResultService.java
+++ src/main/java/pe/edu/pucp/sia/service/StudentResultService.java
@@ -4,6 +4,7 @@ import pe.edu.pucp.sia.model.StudentResult;
 
 public interface StudentResultService {
 	public Iterable<StudentResult> listAll();
+	public Iterable<StudentResult> listBySpeciality(Integer id);
 	public String createStudentResult(StudentResult sr);
 	public String updateStudentResult(StudentResult sr);
 	public String deleteStudentResult(Integer id);
