57036b8a27ebb1cf44a7f1741b7efb98c215f581
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2021-05-13 00:05:12 -0500

    Role services changed
    Role services and implementation changed, now create and update returns id

--- src/main/java/pe/edu/pucp/sia/model/Role.java
+++ src/main/java/pe/edu/pucp/sia/model/Role.java
@@ -15,30 +15,4 @@ public class Role {
     @Id @Column(name="id_role") @GeneratedValue(strategy =GenerationType.AUTO)
     private Integer id;
     private String description;    
-    /*
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public Integer getId(){
-        return this.id;
-    }
-    public void setId(Integer id){
-        this.id=id;
-    }
-    public Activity getActivity(){
-        return this.activity;
-    }
-    public void setActivity(Activity activity){
-        this.activity=activity;
-    }
-    public Faculty getFaculty(){
-        return this.faculty;
-    }
-    public void setFaculty(Faculty faculty){
-        this.faculty=faculty;
-    }
-    */
 }
