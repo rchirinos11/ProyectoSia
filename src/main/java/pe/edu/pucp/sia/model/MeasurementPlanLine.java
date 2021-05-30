9c4a72c3f2917ff084a4b0aeb11b5a3768876321
RJ-code2020 <rjoropeza@pucp.edu.pe>
2021-05-30 14:45:21 -0500

    Correcion MeasurementPlanLine

--- src/main/java/pe/edu/pucp/sia/model/MeasurementPlanLine.java
+++ src/main/java/pe/edu/pucp/sia/model/MeasurementPlanLine.java
@@ -15,7 +15,7 @@ import lombok.Getter;
 @Entity @Getter @Setter
 public class MeasurementPlanLine {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id_person")
+	@Column(name="id_measurement_plan_line")
 	private Integer id;
 	@ManyToOne @JoinColumn(name="id_course")
 	private Course course;
@@ -23,6 +23,9 @@ public class MeasurementPlanLine {
 	private Semester semester;
 	@ManyToOne @JoinColumn(name="id_indicator")
 	private Indicator indicator;
+	@ManyToOne @JoinColumn(name="id_person")
+	private Person person;
+	
 //	@ManyToOne @Column(name="id_section")
 //	private Section section;
 	private int sampleStudents;
