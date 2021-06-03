38a21b3ee0d0bef6dc188c7b8f17a079c96afd8a
Robert <rchirinos@inntechcompany.com>
2021-06-03 17:34:35 -0500

    MPL retorna Id al crear/actualizar

--- src/main/java/pe/edu/pucp/sia/service/MeasurementPlanLineService.java
+++ src/main/java/pe/edu/pucp/sia/service/MeasurementPlanLineService.java
@@ -4,8 +4,8 @@ import pe.edu.pucp.sia.model.MeasurementPlanLine;
 
 public interface MeasurementPlanLineService {
 	public Iterable<MeasurementPlanLine> listAll();
-	public String createMeasurementPlanLine(MeasurementPlanLine m);
-	public String updateMeasurementPlanLine(MeasurementPlanLine m);
+	public Integer createMeasurementPlanLine(MeasurementPlanLine m);
+	public Integer updateMeasurementPlanLine(MeasurementPlanLine m);
 	public String deleteMeasurementPlanLine(Integer id);
 	public Iterable<MeasurementPlanLine> listByCourse(Integer idFaculty);
 	public Iterable<MeasurementPlanLine> listBySpecialtyAndSemester(Integer idSpecialty, Integer idSemester);
