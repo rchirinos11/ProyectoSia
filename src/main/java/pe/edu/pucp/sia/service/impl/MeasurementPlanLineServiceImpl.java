449c25e75c2f9a9bc42de4f688d0c012dd6f5741
Gasper505 <luisgasbor@gmail.com>
2021-06-09 23:53:15 -0500

    cc

--- src/main/java/pe/edu/pucp/sia/service/impl/MeasurementPlanLineServiceImpl.java
+++ src/main/java/pe/edu/pucp/sia/service/impl/MeasurementPlanLineServiceImpl.java
@@ -36,16 +36,32 @@ public class MeasurementPlanLineServiceImpl implements MeasurementPlanLineServic
 	@Override
 	public Integer createMeasurementPlanLine(MeasurementPlanLine m) {
 		Integer response = null;
+		Integer contador=0;
 		try {
 			if(m.getSections()!=null) {
-				for(Section s : m.getSections())
-					if(s.getId()==null)
+				for(Section s : m.getSections()) {
+					if(s.getId()==null) {
 						sectionRepository.save(s);
-			}
-			if(m.getResultsPerCards()!=null) {
+					}
+					contador++;
+				}		
+			}		
+			/*if(m.getResultsPerCards()!=null) {
 				for(ResultsPerCard r : m.getResultsPerCards()) 
 					if(r.getId()==null) 
 						resultsPerCardRepository.save(r).getId();												
+			}*/
+			if(contador!=0) {
+				List<ResultsPerCard> lista= new ArrayList<ResultsPerCard>();				
+				for(Integer i=0;i<contador;i++) {
+					ResultsPerCard r= new ResultsPerCard();	
+					r.setAverage(0.0f);
+					r.setPercentage(0.0f);
+					r.setTotalStudents(0);
+					resultsPerCardRepository.save(r).getId();		
+					lista.add(r);
+				}
+				m.setResultsPerCards(lista);
 			}		
 			response = mPlanLineRepository.save(m).getId();
 		} catch(Exception ex) {
