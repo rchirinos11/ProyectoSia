d8d96f94316689cc075ecfdcf00419f0720abd80
LeoF991203 <cervantesd.lf@pucp.edu.pe>
2021-06-17 18:39:36 -0500

    List_Teachers_By_specialty_TBT
    List_Teachers_By_specialty function. returns a list of Persons (all atributes). To be tested and implemented. Be ready for rollback if needed. Inputs an specialty id and returns a list of teachers

--- src/main/java/pe/edu/pucp/sia/controller/SpecialtyController.java
+++ src/main/java/pe/edu/pucp/sia/controller/SpecialtyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pe.edu.pucp.sia.model.Faculty;
 import pe.edu.pucp.sia.model.Specialty;
+import pe.edu.pucp.sia.model.Person;
 import pe.edu.pucp.sia.requests.SpecialtyUpdateAssistantRequest;
 import pe.edu.pucp.sia.requests.SpecialtyUpdateCoordinatorRequest;
 import pe.edu.pucp.sia.requests.SpecialtyUpdatePercentageRequest;
@@ -88,4 +89,11 @@ public class SpecialtyController {
 		logger.info("Entered method updateSuccessPercentage()");
 		return ResponseEntity.status(HttpStatus.OK).body(specialtyService.updatePercentage(s.getIdSpecialty(),s.getSuccessPercentage()));
 	}
+
+	@GetMapping("/listTeachersBySpecialty/{id}")
+	public ResponseEntity<Object> listTeachersBySpecialty(@PathVariable Integer id){
+		logger.info("Entered method listTeachersBySpecialty()");
+		return ResponseEntity.status(HttpStatus.OK).body(specialtyService.listTeachersBySpecialty(id));
+	}
+
 }
