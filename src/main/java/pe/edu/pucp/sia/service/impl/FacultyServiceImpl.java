bd2fe32423346e665716a7b367e8b1c32fc99c9d
Gasper505 <luisgasbor@gmail.com>
2021-06-18 03:40:53 -0500

    facultad y especialidad
    asignar y desasignar roles al crear/actualizar una facultad/especialidad

--- src/main/java/pe/edu/pucp/sia/service/impl/FacultyServiceImpl.java
+++ src/main/java/pe/edu/pucp/sia/service/impl/FacultyServiceImpl.java
@@ -1,11 +1,18 @@
 package pe.edu.pucp.sia.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pe.edu.pucp.sia.model.Faculty;
+import pe.edu.pucp.sia.model.Person;
+import pe.edu.pucp.sia.model.Role;
 import pe.edu.pucp.sia.model.Specialty;
 import pe.edu.pucp.sia.repository.FacultyRepository;
+import pe.edu.pucp.sia.repository.PersonRepository;
+import pe.edu.pucp.sia.repository.RoleRepository;
 import pe.edu.pucp.sia.response.ApiResponse;
 import pe.edu.pucp.sia.service.FacultyService;
 
@@ -14,6 +21,12 @@ public class FacultyServiceImpl implements FacultyService{
 
 	@Autowired
 	private FacultyRepository facultyRepository;
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Override
 	public ApiResponse listAll() {
@@ -31,6 +44,22 @@ public class FacultyServiceImpl implements FacultyService{
 	public ApiResponse createFaculty(Faculty f) {
 		ApiResponse response = null;
 		try {
+			List<Role> roleList=new ArrayList<Role>();
+			Integer evaluaSiCoordinador=0;
+			if(f.getCoordinator()!=null){
+				Person person =personRepository.findById(f.getCoordinator().getId()).get();
+				roleList=person.getRoleList();
+				for(Role r: roleList) {
+					if(r.getId()==2) {
+						evaluaSiCoordinador=1;
+					}					
+				}		
+				if(evaluaSiCoordinador==0) {
+					roleList.add(roleRepository.findById(2).get());
+					person.setRoleList(roleList);
+					personRepository.save(person);
+				}
+			}		
 			Integer id = facultyRepository.save(f).getId();
 			response = new ApiResponse(id, 201);
 		} catch(Exception ex) {
@@ -43,6 +72,58 @@ public class FacultyServiceImpl implements FacultyService{
 	public ApiResponse updateFaculty(Faculty f) {
 		ApiResponse response = null;
 		try {
+			Person cord=facultyRepository.findById(f.getId()).get().getCoordinator();	
+			List<Role> roleList=new ArrayList<Role>();
+			Integer evaluaSiCoordinador=0;
+			if(f.getCoordinator()!=null){
+				Person person =personRepository.findById(f.getCoordinator().getId()).get();				
+				if(cord!=null) {
+					if(person!=cord) {
+						roleList=person.getRoleList();
+						for(Role r: roleList) {
+							if(r.getId()==2) {
+								evaluaSiCoordinador=1;
+							}					
+						}		
+						if(evaluaSiCoordinador==0) {
+							roleList.add(roleRepository.findById(2).get());
+							person.setRoleList(roleList);
+							personRepository.save(person);
+						}	
+						List <Faculty> fs=facultyRepository.findByCoordinatorId(cord.getId());
+						if(fs.size()==1) {
+							List<Role> cordRoleList=cord.getRoleList();
+							cordRoleList.removeIf(r -> r.getId()==2);
+							cord.setRoleList(cordRoleList);
+							personRepository.save(cord);
+						}
+					}
+				}
+				else {
+					roleList=person.getRoleList();
+					for(Role r: roleList) {
+						if(r.getId()==2) {
+							evaluaSiCoordinador=1;
+						}					
+					}		
+					if(evaluaSiCoordinador==0) {
+						roleList.add(roleRepository.findById(2).get());
+						person.setRoleList(roleList);
+						personRepository.save(person);
+					}	
+				}											
+			}
+			else {
+				if(cord!=null) {
+					List <Faculty> fs=facultyRepository.findByCoordinatorId(cord.getId());
+					if(fs.size()==1) {
+						List<Role> cordRoleList=cord.getRoleList();
+						cordRoleList.removeIf(r -> r.getId()==2);
+						cord.setRoleList(cordRoleList);
+						personRepository.save(cord);
+					}
+				}
+			}			
 			Integer id = facultyRepository.save(f).getId();
 			response = new ApiResponse(id, 200);
 		} catch(Exception ex) {
--- src/main/java/pe/edu/pucp/sia/service/impl/SpecialtyServiceImpl.java
+++ src/main/java/pe/edu/pucp/sia/service/impl/SpecialtyServiceImpl.java
@@ -1,9 +1,17 @@
 package pe.edu.pucp.sia.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pe.edu.pucp.sia.model.Faculty;
+import pe.edu.pucp.sia.model.Person;
+import pe.edu.pucp.sia.model.Role;
 import pe.edu.pucp.sia.model.Specialty;
+import pe.edu.pucp.sia.repository.PersonRepository;
+import pe.edu.pucp.sia.repository.RoleRepository;
 import pe.edu.pucp.sia.repository.SpecialtyRepository;
 import pe.edu.pucp.sia.response.ApiResponse;
 import pe.edu.pucp.sia.service.SpecialtyService;
@@ -12,6 +20,12 @@ import pe.edu.pucp.sia.service.SpecialtyService;
 public class SpecialtyServiceImpl implements SpecialtyService{
 	@Autowired
 	private SpecialtyRepository specialtyRepository;
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Override
 	public ApiResponse listAll(){
@@ -29,6 +43,39 @@ public class SpecialtyServiceImpl implements SpecialtyService{
 	public ApiResponse createSpecialty(Specialty s) {
 		ApiResponse response = null;
 		try {
+			List<Role> roleList1=new ArrayList<Role>();
+			List<Role> roleList2=new ArrayList<Role>();
+			Integer evaulateIfCoordinator=0;
+			Integer evaluateIfAssistant=0;
+			if(s.getCoordinator()!=null){
+				Person person1 =personRepository.findById(s.getCoordinator().getId()).get();
+				roleList1=person1.getRoleList();
+				for(Role r: roleList1) {
+					if(r.getId()==3) {
+						evaulateIfCoordinator=1;
+					}					
+				}		
+				if(evaulateIfCoordinator==0) {
+					roleList1.add(roleRepository.findById(3).get());
+					person1.setRoleList(roleList1);
+					personRepository.save(person1);
+				}
+			}	
+
+			if(s.getAssistant()!=null){
+				Person person2 =personRepository.findById(s.getAssistant().getId()).get();
+				roleList2=person2.getRoleList();
+				for(Role r: roleList2) {
+					if(r.getId()==4) {
+						evaluateIfAssistant=1;
+					}					
+				}		
+				if(evaluateIfAssistant==0) {
+					roleList2.add(roleRepository.findById(4).get());
+					person2.setRoleList(roleList2);
+					personRepository.save(person2);
+				}
+			}			
 			Integer id = specialtyRepository.save(s).getId();
 			response = new ApiResponse(id,201);
 		} catch(Exception ex) {
@@ -41,6 +88,112 @@ public class SpecialtyServiceImpl implements SpecialtyService{
 	public ApiResponse updateSpecialty(Specialty s) {
 		ApiResponse response = null;
 		try {
+			Person cord=specialtyRepository.findById(s.getId()).get().getCoordinator();	
+			List<Role> roleList1=new ArrayList<Role>();
+			Integer evaulateIfCoordinator=0;
+			if(s.getCoordinator()!=null){
+				Person person1 =personRepository.findById(s.getCoordinator().getId()).get();				
+				if(cord!=null) {
+					if(person1!=cord) {
+						roleList1=person1.getRoleList();
+						for(Role r: roleList1) {
+							if(r.getId()==3) {
+								evaulateIfCoordinator=1;
+							}					
+						}		
+						if(evaulateIfCoordinator==0) {
+							roleList1.add(roleRepository.findById(3).get());
+							person1.setRoleList(roleList1);
+							personRepository.save(person1);
+						}	
+						List <Specialty> fs1=specialtyRepository.findByCoordinatorId(cord.getId());
+						if(fs1.size()==1) {
+							List<Role> cordRoleList=cord.getRoleList();
+							cordRoleList.removeIf(r -> r.getId()==3);
+							cord.setRoleList(cordRoleList);
+							personRepository.save(cord);
+						}
+					}
+				}
+				else {
+					roleList1=person1.getRoleList();
+					for(Role r: roleList1) {
+						if(r.getId()==3) {
+							evaulateIfCoordinator=1;
+						}					
+					}		
+					if(evaulateIfCoordinator==0) {
+						roleList1.add(roleRepository.findById(3).get());
+						person1.setRoleList(roleList1);
+						personRepository.save(person1);
+					}	
+				}											
+			}
+			else {
+				if(cord!=null) {
+					List <Specialty> fs1=specialtyRepository.findByCoordinatorId(cord.getId());
+					if(fs1.size()==1) {
+						List<Role> cordRoleList=cord.getRoleList();
+						cordRoleList.removeIf(r -> r.getId()==3);
+						cord.setRoleList(cordRoleList);
+						personRepository.save(cord);
+					}
+				}
+			}	
+			
+			Person ass=specialtyRepository.findById(s.getId()).get().getAssistant();	
+			List<Role> roleList2=new ArrayList<Role>();
+			Integer evaluateIfAssistant=0;
+			if(s.getAssistant()!=null){
+				Person person2 =personRepository.findById(s.getAssistant().getId()).get();				
+				if(ass!=null) {
+					if(person2!=ass) {
+						roleList2=person2.getRoleList();
+						for(Role r: roleList2) {
+							if(r.getId()==4) {
+								evaluateIfAssistant=1;
+							}					
+						}		
+						if(evaluateIfAssistant==0) {
+							roleList2.add(roleRepository.findById(4).get());
+							person2.setRoleList(roleList2);
+							personRepository.save(person2);
+						}	
+						List <Specialty> fs2=specialtyRepository.findByAssistantId(ass.getId());
+						if(fs2.size()==1) {
+							List<Role> assRoleList=ass.getRoleList();
+							assRoleList.removeIf(r -> r.getId()==4);
+							ass.setRoleList(assRoleList);
+							personRepository.save(ass);
+						}
+					}
+				}
+				else {
+					roleList2=person2.getRoleList();
+					for(Role r: roleList2) {
+						if(r.getId()==4) {
+							evaluateIfAssistant=1;
+						}					
+					}		
+					if(evaluateIfAssistant==0) {
+						roleList2.add(roleRepository.findById(4).get());
+						person2.setRoleList(roleList2);
+						personRepository.save(person2);
+					}	
+				}											
+			}
+			else {
+				if(ass!=null) {
+					List <Specialty> fs2=specialtyRepository.findByAssistantId(ass.getId());
+					if(fs2.size()==1) {
+						List<Role> assRoleList=ass.getRoleList();
+						assRoleList.removeIf(r -> r.getId()==4);
+						ass.setRoleList(assRoleList);
+						personRepository.save(ass);
+					}
+				}
+			}	
+			
 			Integer id = specialtyRepository.save(s).getId();
 			response = new ApiResponse(id,200);
 		} catch(Exception ex) {
