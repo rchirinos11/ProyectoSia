9ba30d12ce1abdb798caa1b57b57db1723f6659b
Robert <rchirinos@inntechcompany.com>
2021-06-30 23:35:31 -0500

    Elimina Evidence

--- src/main/java/pe/edu/pucp/sia/repository/EvidenceRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.sia.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pe.edu.pucp.sia.model.Evidence;
-
-
-public interface EvidenceRepository extends CrudRepository<Evidence,Integer>{
-
-}
