165d2b386b6242b9125aaa7bd04d7ee77af2dfd6
RJ-code2020 <rjoropeza@pucp.edu.pe>
2021-07-07 19:41:05 -0500

    Update MPL fix3

--- src/main/java/pe/edu/pucp/sia/repository/ResultsPerCardRepository.java
+++ src/main/java/pe/edu/pucp/sia/repository/ResultsPerCardRepository.java
@@ -17,7 +17,7 @@ public interface ResultsPerCardRepository extends CrudRepository <ResultsPerCard
 	public List<ResultsPerCard> findByMeasurementPlanLineIndicatorStudentResultId(Integer id);
 	public List<ResultsPerCard> findByMeasurementPlanLineIndicatorStudentResultSpecialtyIdAndMeasurementPlanLineSemesterId(
 			Integer idSpecialty, Integer idSemester);
-	public List<ResultsPerCard> findByMeasurementPlanLineIdAndSectionCode(Integer idMpl, int code);
+	public List<ResultsPerCard> findBySectionId(Integer idSection);
 
 	public List<ResultsPerCard> findByMeasurementPlanLineCourseIdAndSectionCode(
 			Integer idCourse, int sectionCode);
